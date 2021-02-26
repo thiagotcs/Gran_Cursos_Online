2035ccf8bd4bc2ccbc625a5c9fe86f488a4bff3d
thiago <thiago.carvalho.ext@brf.com>
2021-02-26 07:36:07 -0300

    feat:added project initial design patterns

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: "all",
+  arrowParens: "avoid",
+
+  printWidth: 100,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  endOfLine: "lf",
+};
